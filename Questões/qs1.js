55627b179e6b9d33c75611220f87c394429bac00
Pedro Souza <pedrosport10@gmail.com>
2024-04-30 20:19:03 -0300

    FIX: Pequena correção na qs1 para input de dados

--- Questões/qs1.js
+++ Questões/qs1.js
@@ -1,8 +1,11 @@
 //Escreva um algoritmo para ler uma temperatura em graus Celsius, calcular e escrever o
 //valor correspondente em graus Fahrenheit.
 
+const prompt = require('prompt-sync')();
+
 const converterTemp = (celsius) => {
     return celsius * 1.8 + 32
 }
 
-console.log(converterTemp(150))
+let entrada = parseFloat(prompt("Temperatura: "));
+console.log(converterTemp(entrada))
