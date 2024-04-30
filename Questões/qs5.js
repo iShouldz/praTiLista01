39aa027a9525f2b97265442a0f628bb198b62793
Pedro Souza <pedrosport10@gmail.com>
2024-04-30 20:35:25 -0300

    FEAT: Adicionando questao 5

---
+++ Questões/qs5.js
@@ -0,0 +1,10 @@
+const prompt = require('prompt-sync')();
+
+let nota1 = parseFloat(prompt("nota 1: "));
+let nota2 = parseFloat(prompt("nota 2: "));
+const media = (nota1 + nota2) / 2
+if(media > 6.0){
+    console.log('PARABÉNS! Você foi aprovado')
+}else{
+    console.log('Você foi REPROVADO! Estude mais')
+}
