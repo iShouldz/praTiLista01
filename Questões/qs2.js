3f37110c5a60bb3500624a346b8df4e7cf756e28
Pedro Souza <pedrosport10@gmail.com>
2024-04-30 20:25:29 -0300

    FEAT: Adicionando questão 2

--- Questões/qs2.js
+++ Questões/qs2.js
@@ -2,3 +2,18 @@
 //votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa
 //em relação ao total de eleitores.
 
+const prompt = require('prompt-sync')();
+
+let total = parseInt(prompt("Total: "));
+let brancos = parseInt(prompt("Brancos: "));
+let nulos = parseInt(prompt("Nulos: "));
+let validos = parseInt(prompt("Validos: "));
+
+if(brancos + nulos + validos !== total){
+    console.log("Valores incogruentes")
+}else{
+    console.log('Porcentagem brancos: ' + (brancos * 100) / total)
+    console.log('Porcentagem nulos: ' + (nulos * 100) / total)
+    console.log('Porcentagem validos: ' + (validos * 100) / total)
+}
+
